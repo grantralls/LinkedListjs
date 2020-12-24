b9330fa8d9ab8a97ef800b120e0ea0bfe3a60b12
gralls <grall001@odu.edu>
2020-12-24 13:41:38 -0500

    testing traverse function

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,17 +1,17 @@
 import assert from 'assert';
 import LinkedList from '../src/index';
 
-const ordered: LinkedList<number> = new LinkedList<number>();
+const ordered = new LinkedList<number>();
 ordered.append(1);
 ordered.append(2);
 ordered.append(3);
 
-const random: LinkedList<number> = new LinkedList<number>();
+const random = new LinkedList<number>();
 random.append(3);
 random.append(1);
 random.append(2);
 
-const backwards: LinkedList<number> = new LinkedList<number>();
+const backwards = new LinkedList<number>();
 backwards.append(3);
 backwards.append(1);
 backwards.append(2);
@@ -19,8 +19,9 @@ backwards.append(2);
 
 describe('Linked List', () => {
     it('should append to end of list', () => {
-        ordered.append(4);
-        assert.strictEqual(ordered.getTailValue(), 4);
+        const mutatedOrdered: LinkedList<number> = ordered;
+        mutatedOrdered.append(4);
+        assert.strictEqual(mutatedOrdered.getTailValue(), 4);
     });
 
     describe('contains()', () => {
@@ -33,6 +34,18 @@ describe('Linked List', () => {
             assert.strictEqual(random.contains(5), false);
             assert.strictEqual(random.contains(0), false);
             assert.strictEqual(random.contains(-1), false);
+        });
+    });
+
+    describe('traverse()', () => {
+        it('should iterate over each item in the list', () => {
+            const orderedIterator: IterableIterator<number> = ordered.traverse();
+            
+            let expectedValue = 1;
+            for(const value of orderedIterator) {
+                assert.strictEqual(value, expectedValue);
+                expectedValue++;
+            }
         });
     });
 });
