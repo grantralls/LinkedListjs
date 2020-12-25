68d440003ce722f2cfe5c2a3bddf8cee48f959ff
gralls <grall001@odu.edu>
2020-12-24 23:51:06 -0500

    ignoring js files in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports =  {
     ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
     sourceType:  'module',  // Allows for the use of imports
   },
-  "ignorePatterns": ["webpack.config.js", "node_modules/", "dist/", "src/*.js"],
+  "ignorePatterns": ["webpack.config.js", "node_modules/", "dist/", "src/*.js", "*.js"],
   rules:  {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. '@typescript-eslint/explicit-function-return-type': 'off',
