0b4075dd2924212a4f911740946bacd346f828dc
gralls <grall001@odu.edu>
2020-12-28 13:25:13 -0500

    updating linked list rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports =  {
   parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
   extends:  [
     'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    'eslint:recommended'
   ],
   parserOptions:  {
     ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
@@ -12,5 +13,9 @@ module.exports =  {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'quotes': [2, 'single', { "avoidEscape": true }]
   },
+  env: {
+      node: true
+  }
 };
