4849ba2852f91a71bc2dd3bd5072d7595a0a0c0d
Grant Ralls <grantsralls@gmail.com>
2020-12-31 11:21:55 -0500

    formatting with prettier

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # Typescript Generics Linked List
-*Author: Grant Ralls <dev@grantralls.net> https://www.grantralls.net*
+
+_Author: Grant Ralls <dev@grantralls.net> https://www.grantralls.net_
 
 ## Foreword
+
 I created this package for the value of practice. I wanted my own implementation of a Linked List using Typescript with Generics to use in future home-projects. Consider this a work in progress, not ready for production.
 
 ---
 
 ## Compatibility
+
 This is designed to be compatible with ES2017. This works best (at the time of writing) with Node 9, 10, and 11.
 
 ---
@@ -14,6 +17,7 @@ This is designed to be compatible with ES2017. This works best (at the time of w
 ## Example
 
 ### Node 10.8.x
+
 ```
 const LinkedList = require("linkedlist").LinkedList;
 
@@ -24,8 +28,9 @@ newList.contains(3); // true
 ```
 
 ### Typescript
+
 ```typescript
-import { LinkedList } from "linkedlist";
+import { LinkedList } from 'linkedlist';
 
 // number can be changed to another type.
 const newList = new LinkedList<number>();
@@ -37,37 +42,46 @@ newList.contains(4); // true
 ---
 
 ## API (Using Typescript)
+
 ### append(value: Type) returns void
+
 The append function adds `value` to the end of the list.
 
 Usage:
+
 ```typescript
 const newList = new LinkedList<number>();
 newList.append(3);
 ```
 
 ### prepend(value: Type) returns void
+
 The prepend function adds `value` to the beginning of the list.
 
 Usage:
+
 ```typescript
 const newList = new LinkedList<number>();
 newList.prepend(3);
 ```
 
 ### copy() returns LinkedList<T>
+
 The copy function deeply copies the list it is being run on.
 
 Usage:
+
 ```typescript
 const newList = new LinkedList<number>();
 const newCopy = newList.copy();
 ```
 
 ### contains(value: type) returns boolean
+
 The contains function searches iteratively for the first instance of `value` in the list.
 
 Usage:
+
 ```typescript
 const newList = new LinkedList<number>();
 newList.append(1);
@@ -78,9 +92,11 @@ newList.contains(5); // false
 ```
 
 ### getHeadValue() returns Type of List
+
 The getHeadValue function returns the data of the head (far-left-end) node of the list. The return type is consistent with the type of the List.
 
 Usage:
+
 ```typescript
 const newList = new LinkedList<number>();
 newList.append(1);
@@ -90,9 +106,11 @@ newList.getHeadValue(); // 1
 ```
 
 ### getTailValue() returns type of List
+
 The getTailValue function returns the data of the tail (far-right-end) node of the list. The return type is consistent with the type of the List.
 
 Usage:
+
 ```typescript
 const newList = new LinkedList<number>();
 newList.append(1);
@@ -102,9 +120,11 @@ newList.getTailValue(); // 2
 ```
 
 ### getSize() returns number
+
 The size function returns the number of nodes in the list.
 
 Usage:
+
 ```typescript
 const newList = new LinkedList<number>();
 newList.append(1);
@@ -114,9 +134,11 @@ newList.getSize(); // 2
 ```
 
 ### removeHead() returns void
+
 The removeHead function removes the current head node from the list. The new head is determined as the next node from the previous head.
 
 Usage:
+
 ```typescript
 const newList = new LinkedList<number>();
 newList.append(1);
@@ -127,10 +149,12 @@ newList.getHeadValue(); // 2
 ```
 
 ### removeTail() returns void
+
 The removeTail function removes the current tail node from the list. The new tail is determined as the previous node from the previous tail.  
-*Note: The runtime for this function is O(n). This list is not doubly linked, therefore it needs to be fully iterated over to find the second to last node.*
+_Note: The runtime for this function is O(n). This list is not doubly linked, therefore it needs to be fully iterated over to find the second to last node._
 
 Usage:
+
 ```typescript
 const newList = new LinkedList<number>();
 newList.append(1);
@@ -141,10 +165,12 @@ newList.getTailValue(); // 1
 ```
 
 ### removeAtIndex(indexOfNodeToRemove: number) returns void
+
 The removeAtIndex function removes the node at the specified index. The two adjacent nodes are then chained together.
-*Note: The runtime for this function is O(n). This list is not doubly linked, therefore it needs to be iterated over to find the node to remove.*
+_Note: The runtime for this function is O(n). This list is not doubly linked, therefore it needs to be iterated over to find the node to remove._
 
 Usage:
+
 ```typescript
 const newList = new LinkedList<number>();
 newList.append(1);
@@ -161,10 +187,12 @@ newList.getSize(); // 2
 ```
 
 ### getValueAtIndex(indexOfNodeDataToRetrieve: number) returns type of List
+
 The getValueAtIndex function returns the data of the node at the index specified.
-*Note: The runtime for this function is O(n). This list is not doubly linked, therefore it needs to be iterated over to find the node to retrieve.*
+_Note: The runtime for this function is O(n). This list is not doubly linked, therefore it needs to be iterated over to find the node to retrieve._
 
 Usage:
+
 ```typescript
 const newList = new LinkedList<number>();
 newList.append(1);
@@ -177,9 +205,11 @@ console.log(result); // 2
 ```
 
 ### traverse() returns IterableIterator
+
 The traverse function is a generator function that returns an Iterable Iterator. See [Javascript Generator Docs](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/function*) for more info.
 
 Usage:
+
 ```typescript
 const newList = new LinkedList<number>();
 newList.append(1);
@@ -187,8 +217,8 @@ newList.append(2);
 
 const listIterator = newList.traverse();
 
-for(const value in listIterator) {
-    console.log(value);
+for (const value in listIterator) {
+	console.log(value);
 }
 
 // Console: 1 2
--- test/index.test.ts
+++ test/index.test.ts
@@ -17,273 +17,305 @@ backwards.append(3);
 backwards.append(2);
 backwards.append(1);
 
-
 describe('Linked List', () => {
-    
-    describe('append()', () => {
+	describe('append()', () => {
+		const mutatedOrdered = ordered.copy();
+		mutatedOrdered.append(4);
 
-        const mutatedOrdered = ordered.copy();
-        mutatedOrdered.append(4);
+		it('should append to end of list', () => {
+			assert.strictEqual(mutatedOrdered.getTailValue(), 4);
+		});
 
-        it('should append to end of list', () => {
-            assert.strictEqual(mutatedOrdered.getTailValue(), 4);
-        });
+		it('should add to the size of the list', () => {
+			assert.strictEqual(mutatedOrdered.getSize(), ordered.getSize() + 1);
+		});
 
-        it('should add to the size of the list', () => {
-            assert.strictEqual(mutatedOrdered.getSize(), ordered.getSize() + 1);
-        });
+		it('should throw an error when attempting to append with no parameter', () => {
+			assert.throws(
+				() => {
+					// @ts-ignore
+					mutatedOrdered.append();
+				},
+				{
+					message: 'Expected some input, got undefined.',
+				},
+				'Should have thrown an error but did not.'
+			);
+		});
+	});
 
-        it('should throw an error when attempting to append with no parameter', () => {
-            assert.throws(() => {
-                // @ts-ignore
-                mutatedOrdered.append();
-            }, {
-                message: 'Expected some input, got undefined.'
-            }, 'Should have thrown an error but did not.');
-        });
+	describe('prepend()', () => {
+		const prependList = random.copy();
+		prependList.prepend(-2);
 
-    });
+		it('should add to the beginning of the list.', () => {
+			assert.strictEqual(prependList.getHeadValue(), -2);
+		});
 
-    describe('prepend()', () => {
+		it('should add to the size of the list', () => {
+			assert.strictEqual(prependList.getSize(), random.getSize() + 1);
+		});
 
-        const prependList = random.copy();
-        prependList.prepend(-2);
+		it('should throw an error when attempting to prepend with no parameter', () => {
+			assert.throws(
+				() => {
+					// @ts-ignore
+					prependList.prepend();
+				},
+				{
+					message: 'Expected some input, got undefined.',
+				},
+				'Should have thrown an error but did not.'
+			);
+		});
+	});
 
-        it('should add to the beginning of the list.', () => {
-            assert.strictEqual(prependList.getHeadValue(), -2);
-        });
+	describe('getSize()', () => {
+		it('should accurately return the number of nodes in the list as a number', () => {
+			const emptyList = new LinkedList<number>();
 
-        it('should add to the size of the list', () => {
-            assert.strictEqual(prependList.getSize(), random.getSize() + 1);
-        });
+			assert.strictEqual(ordered.getSize(), 3);
+			assert.strictEqual(emptyList.getSize(), 0);
+			assert.strictEqual(random.getSize(), 3);
+		});
+	});
 
-        it('should throw an error when attempting to prepend with no parameter', () => {
-            assert.throws(() => {
-                // @ts-ignore
-                prependList.prepend();
-            }, {
-                message: 'Expected some input, got undefined.'
-            }, 'Should have thrown an error but did not.');
-        });
-    });
+	describe('copy()', () => {
+		const deepList = ordered.copy();
 
-    describe('getSize()', () => {
-        it('should accurately return the number of nodes in the list as a number', () => {
-            const emptyList = new LinkedList<number>();
+		it('should create a deep copy of the original list that no longer is equal to the original list', () => {
+			if (deepList == ordered) {
+				assert.fail(
+					'The deep copy is referencing the original, it should not be.'
+				);
+			}
+		});
 
-            assert.strictEqual(ordered.getSize(), 3);
-            assert.strictEqual(emptyList.getSize(), 0);
-            assert.strictEqual(random.getSize(), 3);
-        });
-    });
+		it('should create a deep copy that has the same head value, tail value, and size of the original', () => {
+			const headEquivalency =
+				deepList.getHeadValue() == ordered.getHeadValue();
+			const tailEquivalency =
+				deepList.getTailValue() == ordered.getTailValue();
+			const sizeEquivalency = deepList.getSize() == ordered.getSize();
 
-    describe('copy()', () => {
+			assert.strictEqual(headEquivalency, true);
+			assert.strictEqual(tailEquivalency, true);
+			assert.strictEqual(sizeEquivalency, true);
+		});
 
-        const deepList = ordered.copy();
+		it('should create a deep copy that has the exact same data as the original', () => {
+			const originalListData: Array<number> = [];
+			const deepCopyListData: Array<number> = [];
 
-        it('should create a deep copy of the original list that no longer is equal to the original list', () => {
-            if(deepList == ordered) {
-                assert.fail('The deep copy is referencing the original, it should not be.');
-            }
-        });
+			const orderedIterator = ordered.traverse();
+			const deepIterator = deepList.traverse();
 
-        it('should create a deep copy that has the same head value, tail value, and size of the original', () => {
-            const headEquivalency = (deepList.getHeadValue() == ordered.getHeadValue());
-            const tailEquivalency = (deepList.getTailValue() == ordered.getTailValue());
-            const sizeEquivalency = (deepList.getSize() == ordered.getSize());
+			for (const value of orderedIterator) {
+				originalListData.push(value);
+			}
 
-            assert.strictEqual(headEquivalency, true);
-            assert.strictEqual(tailEquivalency, true);
-            assert.strictEqual(sizeEquivalency, true);
-        });
+			for (const value of deepIterator) {
+				deepCopyListData.push(value);
+			}
 
-        it('should create a deep copy that has the exact same data as the original', () => {
-            const originalListData: Array<number> = [];
-            const deepCopyListData: Array<number> = [];
+			for (let i = 0; i < originalListData.length; i++) {
+				if (originalListData[i] != deepCopyListData[i]) {
+					assert.fail(
+						'The data within the deep-copied list is not equal to that of the original list'
+					);
+				}
+			}
+		});
+	});
 
-            const orderedIterator = ordered.traverse();
-            const deepIterator = deepList.traverse();
+	describe('getAtIndex()', () => {
+		const indexList = random.copy();
 
-            for(const value of orderedIterator) {
-                originalListData.push(value);
-            }
+		it('should return the data at the node accurately on a non-empty list', () => {
+			assert.strictEqual(indexList.getValueAtIndex(0), 3);
+			assert.strictEqual(indexList.getValueAtIndex(1), 1);
+			assert.strictEqual(indexList.getValueAtIndex(2), 2);
+		});
 
-            for(const value of deepIterator) {
-                deepCopyListData.push(value);
-            }
+		it('should throw an error when attempting to access index 0 on an empty list', () => {
+			const emptyList = new LinkedList<number>();
 
-            for(let i = 0; i < originalListData.length; i++) {
-                if(originalListData[i] != deepCopyListData[i]) {
-                    assert.fail('The data within the deep-copied list is not equal to that of the original list');
-                }
-            }
-            
-        });
-    });
+			assert.throws(
+				() => {
+					emptyList.getValueAtIndex(0);
+				},
+				{
+					message:
+						'Received an index larger than the size of the list.',
+				},
+				'Should have thrown an error but did not.'
+			);
+		});
 
-    describe('getAtIndex()', () => {
+		it('should throw an error when attempting to retrieve the value at an index that goes beyond the positive bounds of the list', () => {
+			assert.throws(
+				() => {
+					indexList.getValueAtIndex(4);
+				},
+				{
+					message:
+						'Received an index larger than the size of the list.',
+				},
+				'Should have thrown an error but did not.'
+			);
+		});
 
-        const indexList = random.copy();
+		it('should throw an error when attempting to retrieve the value at an index below 0', () => {
+			assert.throws(
+				() => {
+					indexList.getValueAtIndex(-1);
+				},
+				{
+					message:
+						'Received a negative number, expected a positive number.',
+				},
+				'Should have thrown an error but did not.'
+			);
+		});
 
-        it('should return the data at the node accurately on a non-empty list', () => {
-            assert.strictEqual(indexList.getValueAtIndex(0), 3);
-            assert.strictEqual(indexList.getValueAtIndex(1), 1);
-            assert.strictEqual(indexList.getValueAtIndex(2), 2);
-        });
+		it('should throw an error when attempting to retrieve the value at an index that is a decimal number', () => {
+			assert.throws(
+				() => {
+					indexList.getValueAtIndex(1.5);
+				},
+				{
+					message: 'Received a decimal, expected a natural number.',
+				},
+				'Should have thrown an error but did not.'
+			);
+		});
+	});
 
-        it('should throw an error when attempting to access index 0 on an empty list', () => {
-            const emptyList = new LinkedList<number>();
-            
-            assert.throws(() => {
-                emptyList.getValueAtIndex(0);
-            }, {
-                message: 'Received an index larger than the size of the list.'
-            }, 'Should have thrown an error but did not.');
-        });
+	describe('getHeadValue()', () => {
+		it('should return the data of the node at the head', () => {
+			assert.strictEqual(ordered.getHeadValue(), 1);
+			assert.strictEqual(backwards.getHeadValue(), 3);
+			assert.strictEqual(random.getHeadValue(), 3);
+		});
+		it('should return undefined if the list is empty', () => {
+			const emptyList = new LinkedList<number>();
 
-        it('should throw an error when attempting to retrieve the value at an index that goes beyond the positive bounds of the list', () => {
-            assert.throws(() => {
-                indexList.getValueAtIndex(4);
-            }, {
-                message: 'Received an index larger than the size of the list.'
-            }, 'Should have thrown an error but did not.');
-        });
+			assert.strictEqual(emptyList.getHeadValue(), undefined);
+		});
+	});
 
-        it('should throw an error when attempting to retrieve the value at an index below 0', () => {
-            assert.throws(() => {
-                indexList.getValueAtIndex(-1);
-            }, {
-                message: 'Received a negative number, expected a positive number.'
-            }, 'Should have thrown an error but did not.');
-        });
+	describe('getTailValue()', () => {
+		it('should return the data of the node at the tail', () => {
+			assert.strictEqual(ordered.getTailValue(), 3);
+			assert.strictEqual(backwards.getTailValue(), 1);
+			assert.strictEqual(random.getTailValue(), 2);
+		});
+		it('should return undefined if the list is empty', () => {
+			const emptyList = new LinkedList<number>();
 
-        it('should throw an error when attempting to retrieve the value at an index that is a decimal number', () => {
-            assert.throws(() => {
-                indexList.getValueAtIndex(1.5);
-            }, {
-                message: 'Received a decimal, expected a natural number.'
-            }, 'Should have thrown an error but did not.');
-        });
-    });
+			assert.strictEqual(emptyList.getTailValue(), undefined);
+		});
+	});
 
-    describe('getHeadValue()', () => {
-        it('should return the data of the node at the head', () => {
-            assert.strictEqual(ordered.getHeadValue(), 1);
-            assert.strictEqual(backwards.getHeadValue(), 3);
-            assert.strictEqual(random.getHeadValue(), 3);
-        });
-        it('should return undefined if the list is empty', () => {
-            const emptyList = new LinkedList<number>();
+	describe('removeAtIndex()', () => {
+		const removedList = ordered.copy();
 
-            assert.strictEqual(emptyList.getHeadValue(), undefined);
-        });
-    });
+		const singleList = new LinkedList<number>();
+		singleList.append(1);
 
-    describe('getTailValue()', () => {
-        it('should return the data of the node at the tail', () => {
-            assert.strictEqual(ordered.getTailValue(), 3);
-            assert.strictEqual(backwards.getTailValue(), 1);
-            assert.strictEqual(random.getTailValue(), 2);
-        });
-        it('should return undefined if the list is empty', () => {
-            const emptyList = new LinkedList<number>();
+		it('should make the list empty when removing the last item in the list', () => {
+			singleList.removeAtIndex(0);
 
-            assert.strictEqual(emptyList.getTailValue(), undefined);
-        });
-    });
+			assert.strictEqual(singleList.getSize(), 0);
+			assert.strictEqual(singleList.getHeadValue(), undefined);
+			assert.strictEqual(singleList.getTailValue(), undefined);
+		});
 
-    describe('removeAtIndex()', () => {
-        const removedList = ordered.copy();
-        
-        const singleList = new LinkedList<number>();
-        singleList.append(1);
+		it('should remove the node from the proper index', () => {
+			removedList.removeAtIndex(1);
+			const solution = [1, 3];
 
-        it('should make the list empty when removing the last item in the list', () => {
-            singleList.removeAtIndex(0);
+			for (let i = 0; i < removedList.getSize(); i++) {
+				assert.strictEqual(removedList.getValueAtIndex(i), solution[i]);
+			}
 
-            assert.strictEqual(singleList.getSize(), 0);
-            assert.strictEqual(singleList.getHeadValue(), undefined);
-            assert.strictEqual(singleList.getTailValue(), undefined);
-        });
+			assert.strictEqual(removedList.getSize(), ordered.getSize() - 1);
+		});
+	});
 
-        it('should remove the node from the proper index', () => {
-            removedList.removeAtIndex(1);
-            const solution = [1, 3];
+	describe('removeHead()', () => {
+		const removeList = random.copy();
+		removeList.removeHead();
 
-            for(let i = 0; i < removedList.getSize(); i++) {
-                assert.strictEqual(removedList.getValueAtIndex(i), solution[i]);
-            }
+		it('should remove the head node from the list', () => {
+			assert.strictEqual(removeList.getHeadValue(), 1);
+		});
 
-            assert.strictEqual(removedList.getSize(), ordered.getSize() - 1);
-        });
-    });
+		it('should have a size of 1 less than the original', () => {
+			assert.strictEqual(removeList.getSize(), random.getSize() - 1);
+		});
 
-    describe('removeHead()', () => {
-        const removeList = random.copy();
-        removeList.removeHead();
+		it('should have the same tail value as the original', () => {
+			assert.strictEqual(
+				removeList.getTailValue(),
+				random.getTailValue()
+			);
+		});
+	});
 
-        it('should remove the head node from the list', () => {
-            assert.strictEqual(removeList.getHeadValue(), 1);
-        });
+	describe('removeTail()', () => {
+		const removeList = random.copy();
+		removeList.removeTail();
 
-        it('should have a size of 1 less than the original', () => {
-            assert.strictEqual(removeList.getSize(), random.getSize() - 1);
-        });
+		it('should have the same head value as the original', () => {
+			assert.strictEqual(removeList.getHeadValue(), 3);
+		});
 
-        it('should have the same tail value as the original', () => {
-            assert.strictEqual(removeList.getTailValue(), random.getTailValue());
-        });
-    });
+		it('should have a size of 1 less than the original', () => {
+			assert.strictEqual(removeList.getSize(), random.getSize() - 1);
+		});
 
-    describe('removeTail()', () => {
-        const removeList = random.copy();
-        removeList.removeTail();
+		it('should remove the tail node from the list', () => {
+			assert.strictEqual(removeList.getTailValue(), 1);
+		});
+	});
 
-        it('should have the same head value as the original', () => {
-            assert.strictEqual(removeList.getHeadValue(), 3);
-        });
+	describe('contains()', () => {
+		it('should return true if the value is in the list', () => {
+			assert.strictEqual(random.contains(2), true);
+			assert.strictEqual(random.contains(1), true);
+			assert.strictEqual(random.contains(3), true);
+		});
 
-        it('should have a size of 1 less than the original', () => {
-            assert.strictEqual(removeList.getSize(), random.getSize() - 1);
-        });
+		it('should return false if the value is not in the list', () => {
+			assert.strictEqual(random.contains(5), false);
+			assert.strictEqual(random.contains(0), false);
+			assert.strictEqual(random.contains(-1), false);
+		});
 
-        it('should remove the tail node from the list', () => {
-            assert.strictEqual(removeList.getTailValue(), 1);
-        });
-    });
+		it('should throw an error when attempting contains with no parameter', () => {
+			assert.throws(
+				() => {
+					// @ts-ignore
+					random.contains();
+				},
+				{
+					message: 'Expected some input, got undefined.',
+				},
+				'Should have thrown an error but did not.'
+			);
+		});
+	});
 
-    describe('contains()', () => {
-        it('should return true if the value is in the list', () => {
-            assert.strictEqual(random.contains(2), true);
-            assert.strictEqual(random.contains(1), true);
-            assert.strictEqual(random.contains(3), true);
-        });
+	describe('traverse()', () => {
+		it('should iterate over each item in the list', () => {
+			const orderedIterator: IterableIterator<number> = ordered.traverse();
 
-        it('should return false if the value is not in the list', () => {
-            assert.strictEqual(random.contains(5), false);
-            assert.strictEqual(random.contains(0), false);
-            assert.strictEqual(random.contains(-1), false);
-        });
-
-        it('should throw an error when attempting contains with no parameter', () => {
-            assert.throws(() => {
-                // @ts-ignore
-                random.contains();
-            }, {
-                message: 'Expected some input, got undefined.'
-            }, 'Should have thrown an error but did not.');
-        });
-    });
-
-    describe('traverse()', () => {
-        it('should iterate over each item in the list', () => {
-            const orderedIterator: IterableIterator<number> = ordered.traverse();
-            
-            let expectedValue = 1;
-            for(const value of orderedIterator) {
-                assert.strictEqual(value, expectedValue);
-                expectedValue++;
-            }
-        });
-    });
+			let expectedValue = 1;
+			for (const value of orderedIterator) {
+				assert.strictEqual(value, expectedValue);
+				expectedValue++;
+			}
+		});
+	});
 });
