569c5cf5729665018bae6dc49cb8a3616e08c615
Grant Ralls <dev@grantralls.net>
2021-12-25 23:23:35 -0500

    making variables private as needed

--- src/linkedlist.ts
+++ src/linkedlist.ts
@@ -1,9 +1,9 @@
 import { Node } from './node';
 
 export class LinkedList<T> {
-    protected head: Node<T> | undefined;
-    protected tail: Node<T> | undefined;
-    protected size = 0;
+    private head: Node<T> | undefined;
+    private tail: Node<T> | undefined;
+    private size = 0;
 
     /**
      * Usage:
